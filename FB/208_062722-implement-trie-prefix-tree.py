7a1c0f28bf1a1d1537aa699a8fe3ba7ad1dd11d8
Tae Hee Choi <dadac76@hotmail.com>
2022-06-28 07:55:39 -0700

    review

---
+++ FB/208_062722-implement-trie-prefix-tree.py
@@ -0,0 +1,73 @@
+# https://leetcode.com/problems/implement-trie-prefix-tree/discuss/1804957/Python-3-Easy-solution
+
+
+class Trie:
+
+    def __init__(self):
+        self.children = {}
+        
+        self.is_end = False
+
+    def insert(self, word):
+        curr = self
+        for w in word:
+            if w not in curr.children:
+                curr.children[w] = Trie()
+                print(curr.children)
+
+            curr = curr.children[w]
+        curr.is_end = True
+
+    def search(self, word):
+        curr = self
+        for w in word:
+            if w not in curr.children:
+                return False
+            curr = curr.children[w]
+        return curr.is_end
+
+    def startsWith(self, prefix):
+        curr = self
+        for w in prefix:
+            if w not in curr.children:
+                return False
+            curr = curr.children[w]
+        return True
+
+obj = Trie()
+obj.insert("app")
+
+obj.insert("apple")
+# obj.insert("apple")
+
+# print(obj.search("apple"))
+# print(obj.search("app"))
+# print(obj.startsWith("app"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
